7e1e9ee5b3a6bb6a8598a311470be277925a17ba
Anu Kandasamy <kandasamy.a@husky.neu.edu>
2020-05-10 17:29:58 -0400

    initial commit

---
+++ cs4550-2020summer1-anuk1998-server-java/src/main/java/com/example/cs45502020summer1anuk1998serverjava/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs45502020summer1anuk1998serverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+}
---
+++ cs4550-2020summer1-anuk1998-server-java/src/test/java/com/example/cs45502020summer1anuk1998serverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs45502020summer1anuk1998serverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
