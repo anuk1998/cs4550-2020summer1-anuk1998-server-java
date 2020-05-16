dad50ea2da0d8df310419c515ecd36dcb1da37ea
Anu Kandasamy <kandasamy.a@husky.neu.edu>
2020-05-15 23:44:48 -0400

    started using jQuery

---
+++ cs4550-2020summer1-anuk1998-server-java/src/main/webapp/user-admin/user-admin.controller.client.js
@@ -0,0 +1,24 @@
+const users =[
+  {username: 'alice', password: 'alice', first: 'Alice', last: 'Wonderland', role: 'Faculty'}
+  {username: 'bob', password: 'bob', first: 'Bob', last: 'Marley', role: 'Faculty'}
+  {username: 'charlie', password: 'charlie', first: 'Charlie', last: 'Garcia', role: 'Student'}
+
+]
+
+function init() {
+//fetch all H1 elements from HTML document
+const h1 = jQuery('h1')
+h1.css('color', 'red')
+}
+
+const tr = jQuery('tr')
+tr.css('backgroundColor', blue)
+
+jQuery(init)
+
+
+
+
+
+
+
