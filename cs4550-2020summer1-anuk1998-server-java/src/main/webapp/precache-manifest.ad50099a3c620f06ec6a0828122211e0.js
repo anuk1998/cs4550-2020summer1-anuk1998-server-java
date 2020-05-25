6703d6a3ffec269166a1c4abb9c9aa306b3fa74a
Anu Kandasamy <kandasamy.a@husky.neu.edu>
2020-05-25 15:05:19 -0400

    added react

---
+++ cs4550-2020summer1-anuk1998-server-java/src/main/webapp/precache-manifest.ad50099a3c620f06ec6a0828122211e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3419ed2857a4f286e6e0a0bc1fa5c43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eb0c78e8c802dbf5b82",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8105e0a7712fa8271676",
+    "url": "/static/css/main.6aa5b1e0.chunk.css"
+  },
+  {
+    "revision": "7eb0c78e8c802dbf5b82",
+    "url": "/static/js/2.cc1a9a84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cc1a9a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8105e0a7712fa8271676",
+    "url": "/static/js/main.fcc31392.chunk.js"
+  },
+  {
+    "revision": "01f4da542f117f28d7ec",
+    "url": "/static/js/runtime-main.95da8e1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
