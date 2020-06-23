8b6d64f2ee6408e73939d94f1e53024698c0edf3
Anu Kandasamy <kandasamy.a@husky.neu.edu>
2020-06-22 23:03:49 -0400

    npm build

--- cs4550-2020summer1-anuk1998-server-java/src/main/java/com/example/cs45502020summer1anuk1998serverjava/controllers/HelloWorldController.java
+++ cs4550-2020summer1-anuk1998-server-java/src/main/java/com/example/cs45502020summer1anuk1998serverjava/controllers/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
+public class HelloWorldController {  
     @GetMapping("/api/hello/{message}")
     public HelloModel sayHello(
             @PathVariable("message") String msg) {
